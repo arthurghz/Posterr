f0a049aa430a77a2465d24921caf1585927c6122
arthur souza <arthur.ghz@icloud.com>
2023-08-05 18:56:19 -0300

    Add web server gateway interface

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,12 @@
-import os
+
 from flask import Flask
-from src.models import db
-from src.views import user_view, post_view
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+from .config import Config
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    db.init_app(app)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    app.register_blueprint(user_view)
-    app.register_blueprint(post_view)
-    return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=8080)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
---
+++ src/config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
