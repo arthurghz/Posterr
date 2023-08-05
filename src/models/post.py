ad1efc2c57a5c815d8753e6350fcf3ab9547c9bb
arthur souza <arthur.ghz@icloud.com>
2023-08-05 18:59:56 -0300

    refactor models and add encondig type

--- src/models/post.py
+++ src/models/post.py
@@ -1,17 +1,33 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from sqlalchemy import ForeignKey
+
 from . import db
+from .user import User
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    content = db.Column(db.String(777))
-    post_date = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    original_post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=True)
-    quote = db.Column(db.String(777))
+    '''Post Model'''
+    __tablename__ = 'post'
+
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    repost_from_id = db.Column(db.BigInteger, ForeignKey('post.id'), nullable=True)
+    quote_from_id = db.Column(db.BigInteger, ForeignKey('post.id'), nullable=True)
+    user_id = db.Column(db.BigInteger, ForeignKey('user.id'), nullable=False)
+    datetime_creation = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    text = db.Column(db.String(777), nullable=True)
+    is_deleted = db.Column(db.Boolean, default=False)
+
+    user = db.relationship('User', backref='posts')
+    repost = db.relationship('Post', remote_side=[id], foreign_keys=[repost_from_id], backref='reposts')
+    quote = db.relationship('Post', remote_side=[id], foreign_keys=[quote_from_id], backref='quotes')
+
     def to_dict(self):
         return {
             'id': self.id,
-            'content': self.content,
-            'post_date': self.post_date.isoformat() if self.post_date else None,
-            'original_post_id': self.original_post_id,
-            'quote': self.quote
+            'user_id': self.user_id,
+            'repost_from_id': self.repost_from_id,
+            'quote_from_id': self.quote_from_id,
+            'datetime_creation': self.datetime_creation.strftime('%Y-%m-%d %H:%M:%S') if self.datetime_creation else None,
+            'text': self.text,
+            'is_deleted': self.is_deleted,
         }
--- src/models/user.py
+++ src/models/user.py
@@ -1,18 +1,26 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime
+from sqlalchemy.sql import text
 
 from . import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(14), unique=True)
-    joined_date = db.Column(db.DateTime)
-    posts = db.relationship('Post', backref='User', lazy=True)
+    __tablename__ = 'user'
+
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(14), unique=True, nullable=False)
+    date_joined = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='user', lazy=True)
+
+    def __init__(self, username):
+        self.username = username
 
     def to_dict(self):
         return {
             'id': self.id,
-            'joined_date': self.joined_date.strftime('%Y-%m-%d %H:%M:%S') if self.joined_date else None,
+            'username': self.username,
+            'date_joined': self.date_joined.strftime('%Y-%m-%d') if self.date_joined else None,
             'posts': [post.to_dict() for post in sorted(self.posts, key=lambda p: p.post_date, reverse=True)[:5]],
             'total_posts': len(self.posts)
         }
